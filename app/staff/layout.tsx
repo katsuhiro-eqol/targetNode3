4c8a1860f70625be3ebfbd4ad36ec10181d43a3c
katsuhiro-eqol <katsuhiro.yamashita.sr01@gmail.com>
2025-06-13 10:31:49 +0900

    20250613

--- app/staff/layout.tsx
+++ app/staff/layout.tsx
@@ -1,7 +1,7 @@
 "use client"
 import React from "react";
 
-export default function Rayout({children}:{children:React.ReactNode}){
+export default function Layout({children}:{children:React.ReactNode}){
     return (
         <div className="flex">
         <div className="p-8 w-full">
